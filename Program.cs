f10d0fae13ca1e05b7763a65cf0fe81c5d04b5e3
Hiten <hitendra100282@gmail.com>
2019-09-06 18:50:54 +0530

    testing

--- Program.cs
+++ Program.cs
@@ -7,6 +7,22 @@ namespace C_Sharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter depth of piramid in integers only");
+            //int depth = Convert.ToInt16(Console.ReadLine());
+            BuildPiramid(5);
+            Console.ReadKey();
+        }
+
+        static void BuildPiramid(int depth)
+        {
+            for(int i = 0; i<depth;i++)
+            {
+                Console.WriteLine();
+                for(int j=0;j<i;j++)
+                {
+                     Console.Write("*");
+                }
+            }
         }
     }
 }
